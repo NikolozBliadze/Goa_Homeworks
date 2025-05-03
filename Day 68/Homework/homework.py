d9af12e7902f65cbadc880c75e88db2037cb4660
NikolozBliadze <bliadzen19@gmail.com>
2025-05-03 21:41:03 +0400

    Commit

---
+++ Day 68/Homework/homework.py
@@ -0,0 +1,16 @@
+# 1
+doubleNumber = lambda num : num * 2
+print(doubleNumber(3))
+
+# 2
+count = lambda word : len(word)
+print(count("Keti"))
+
+# 3
+EvenOdd = lambda num : "Even" if num % 2 == 0 else "Odd"
+print(EvenOdd(5))
+print(EvenOdd(6))
+
+# 4
+cube = lambda num : num ** 2
+print(cube(3))
